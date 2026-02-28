997400f271781328848f2cf6448ce306550e4706
Amirhosein Sebghati <amir.sebghati85@gmail.com>
2026-02-28 04:03:50 +0330

    aaa

--- practice_project/guess_the_number.cs
+++ practice_project/guess_the_number.cs
@@ -1,65 +1,75 @@
-﻿Console.WriteLine("Guess The Number!");
+﻿using System;
 
-Random random = new Random();
-
-bool playAgain = true;
-int min = 1;
-int max = 10;
-int guess;
-int number;
-int guesses;
-
-while (playAgain)
+namespace practice_project
 {
-    guess = 0;
-    guesses = 0;
-    number = random.Next(min, max + 1);
-
-    while (guess != number)
+    class guess_the_number
     {
-        Console.Write("Guess a Number between " + min + " and " + max + " : ");
-        guess = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Guessed Number: " + guess);
-
-        if (guess > number)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Guess a lower number!");
-            guesses++;
-        }
-        else if (guess < number)
-        {
-            Console.WriteLine("Guess a higher number!");
-            guesses++;
-        }
-        Console.WriteLine("--------------------------");
+            Console.WriteLine("Guess The Number!");
 
-    }
-    guesses++;
-    Console.WriteLine("Correct! The number was: " + number);
-    Console.WriteLine("Your had " + guesses + " guesses!");
-    guesses = 0;
+            Random random = new Random();
+
+            bool playAgain = true;
+            int min = 1;
+            int max = 10;
+            int guess;
+            int number;
+            int guesses;
+
+            while (playAgain)
+            {
+                guess = 0;
+                guesses = 0;
+                number = random.Next(min, max + 1);
+
+                while (guess != number)
+                {
+                    Console.Write("Guess a Number between " + min + " and " + max + " : ");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Guessed Number: " + guess);
+
+                    if (guess > number)
+                    {
+                        Console.WriteLine("Guess a lower number!");
+                    }
+                    else if (guess < number)
+                    {
+                        Console.WriteLine("Guess a higher number!");
+                    }
+                    guesses++;
+                    Console.WriteLine("--------------------------");
+
+                }
+                Console.WriteLine("Correct! The number was: " + number);
+                Console.WriteLine("Your had " + guesses + " guesses!");
+                guesses = 0;
 
 
-    bool gave_answer = false;
-    while (gave_answer == false)
-    {
-        Console.Write("Would you like to play again?");
-        Console.WriteLine();
-        String answer = Console.ReadLine();
+                bool gave_answer = false;
+                while (gave_answer == false)
+                {
+                    Console.Write("Would you like to play again?");
+                    Console.WriteLine();
+                    String answer;
+                    answer = Console.ReadLine().ToUpper();
 
-        if (answer == "Yes" || answer == "yes")
-        {
-            playAgain = true;
-            gave_answer = true;
-        }
-        else if (answer == "No" || answer == "no")
-        {
-            playAgain = false;
-            gave_answer = true;
-        }
-        else
-        {
-            Console.WriteLine("Please type Yes/yes OR No/no: ");
+                    if (answer == "YES")
+                    {
+                        playAgain = true;
+                        gave_answer = true;
+                    }
+                    else if (answer == "NO")
+                    {
+                        playAgain = false;
+                        gave_answer = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please type Yes OR No: ");
+                    }
+                }
+            }
         }
     }
 }
